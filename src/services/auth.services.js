9a7f5389f59b5cbd1956845d677515e78240b77c
Sparsh Jha <sparshjha40@gmail.com>
2024-07-13 00:26:08 +0530

    updated gitignore and switched from bcrypt to bcryptjs

--- src/services/auth.services.js
+++ src/services/auth.services.js
@@ -1,12 +1,17 @@
 const jwt = require("jsonwebtoken");
 const msg = require("../common/messages");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const userData = require("../models/userModel");
 
 async function userSignup(data, res) {
   try {
     const { username, email, password } = data;
-    const hashedPass = await bcrypt.hash(password, 10);
+    let hashedPass = "";
+    bcrypt.genSalt(10, function (err, salt) {
+      bcrypt.hash(password, salt, function (err, hash) {
+        hashedPass = hash;
+      });
+    });
     const newUser = new userData({
       username,
       email,
@@ -28,16 +33,26 @@ async function userLogin(username, password, res) {
     if (!user) {
       return res.status(400).json({ error: msg.AUTH_FAIL });
     }
-    const passwordMatch = await bcrypt.compare(password, user.password);
-    if (!passwordMatch) {
-      return res.status(401).json({ error: msg.AUTH_FAIL });
-    }
+    let hashedPass = "";
+    bcrypt.genSalt(10, function (err, salt) {
+      bcrypt.hash(password, salt, function (err, hash) {
+        if (err) throw new Error(err);
+        hashedPass = hash;
+        bcrypt.compare(password, hash, function (err, res) {
+          if (!res) {
+            if (err) throw new Error(err);
+            return res.status(401).json({ error: msg.AUTH_FAIL });
+          }
+        });
+      });
+    });
+
     const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
       expiresIn: "1h",
     });
     res.status(200).json({ token });
   } catch (error) {
-    console.log(msg.AUTH_FAILED);
+    res.status(500).json({ error: error });
     console.error(error);
     return null;
   }
